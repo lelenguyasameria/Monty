2314ce0ecf0ec1a4956117b3e73ebda276e7a9b9
lelenguyasameria <somearia711@gmail.com>
2023-08-27 14:47:26 -0700

    task

--- add.c
+++ add.c
@@ -1,19 +1,32 @@
 #include "monty.h"
-
 /**
- * add - Adds the top two elements of the stack
- * @stack: Double pointer to the head of the stack
- * @line_number: Line number in the file
- */
-void add(stack_t **stack, unsigned int line_number)
+ * f_add - adds the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_add(stack_t **head, unsigned int counter)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	stack_t *h;
+	int len = 0, aux;
 
-    (*stack)->next->data += (*stack)->data;
-    nop(stack, line_number);
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->n + h->next->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
 }
-
--- pint.c
+++ pint.c
@@ -1,18 +1,19 @@
 #include "monty.h"
-
 /**
- * pint - Prints the value at the top of the stack.
- * @stack: Pointer to the head of the stack.
- * @line_number: Line number of the instruction.
- */
-void pint(stack_t **stack, unsigned int line_number)
+ * f_pint - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_pint(stack_t **head, unsigned int counter)
 {
-    if (!*stack)
-    {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("%d\n", (*stack)->data);
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
 }
-
